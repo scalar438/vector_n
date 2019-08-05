0ef2716ce2dc57f9ba2c36750007093693859940
Alexander Larin <scalar438@gmail.com>
2019-08-05 09:24:05 +0300

    Remove precompiled header

--- vector_n/vector_n/main.cpp
+++ vector_n/vector_n/main.cpp
@@ -1,7 +1,6 @@
 // vector_n.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
 #include "vector_n.h"
 #include <omp.h>
